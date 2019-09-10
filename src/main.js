5dc16988874338088641b7806d51096ab360e073
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2019-09-10 16:39:43 -0600

    CORS add components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { createProvider } from './vue-apollo'
+import { createProvider } from "./vue-apollo";
 
 Vue.config.productionTip = false;
 
