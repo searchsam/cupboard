427a12dd42cfd22f6a5e330718a407b8586cec86
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2019-09-25 15:24:20 -0600

    Add policies

---
+++ app/Policies/RequestPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Request;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class RequestPolicy
+{
+    use HandlesAuthorization;
+    
+    /**
+     * Determine whether the user can view any requests.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the request.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Request  $request
+     * @return mixed
+     */
+    public function view(User $user, Request $request)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create requests.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the request.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Request  $request
+     * @return mixed
+     */
+    public function update(User $user, Request $request)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the request.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Request  $request
+     * @return mixed
+     */
+    public function delete(User $user, Request $request)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the request.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Request  $request
+     * @return mixed
+     */
+    public function restore(User $user, Request $request)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the request.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Request  $request
+     * @return mixed
+     */
+    public function forceDelete(User $user, Request $request)
+    {
+        //
+    }
+}
