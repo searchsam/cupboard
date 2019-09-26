2d5c26f7b53f0dc1ed157aad1d2e170bab9cd6db
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2019-09-25 18:06:35 -0600

    refactor order view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex h-full">
-      <div class="w-1/7"><NavBar /></div>
-      <div class="w-6/7 w-full m-5"><RouterView /></div>
-    </div>
+  <div class="flex h-full">
+    <div class="w-1/7"><NavBar /></div>
+    <div class="w-6/7 w-full m-5"><RouterView /></div>
+  </div>
 </template>
 
 <script type="text/javascript">
--- src/views/Request.vue
+++ src/views/Request.vue
@@ -1,36 +1,43 @@
 <template>
-    <div id="order">
-      <h1 class="font-thin text-6xl">
-        Solicitudes {{ this.$route.params.id }}
-      </h1>
+  <div id="order">
+    <h1 class="font-thin text-6xl">Solicitudes</h1>
 
-      <!-- Create Request Form -->
-      <div id="createRequestForm" class="w-full bg-white m-2" v-if="orderStatus">
-        <h1 class="p-4 text-xl">Crear Nueva Solicitud</h1>
-        <form class="p-4" action="" method="POST" @submit.prevent="createRequest">
-          <input class="mr-2 h-12 w-64 border-2 border-gray-400 placeholder-gray-400" v-model="description" type="text" placeholder="Peticion">
-          <input class="mr-2 h-12 border-2 border-gray-400 placeholder-gray-400" v-model="quantity" type="text" placeholder="Cantidad">
-          <button
-            class="bg-yellow-500 text-white text-xl h-12 w-40 hover:bg-yellow-400"
-            type="submit"
-          >
-            Crear Solicitud
-          </button>
-          <Alert :msg="error" />
-        </form>
-      </div>
+    <!-- Create Request Form -->
+    <div id="createRequestForm" class="w-full bg-white m-2" v-if="orderStatus">
+      <h1 class="p-4 text-xl">Crear Nueva Solicitud</h1>
+      <form class="p-4" action="" method="POST" @submit.prevent="createRequest">
+        <input
+          class="mr-2 h-12 w-64 border-2 border-gray-400 placeholder-gray-400"
+          v-model="description"
+          type="text"
+          placeholder="Peticion"
+        />
+        <input
+          class="mr-2 h-12 border-2 border-gray-400 placeholder-gray-400"
+          v-model="quantity"
+          type="text"
+          placeholder="Cantidad"
+        />
+        <button
+          class="bg-yellow-500 text-white text-xl h-12 w-40 hover:bg-yellow-400"
+          type="submit"
+        >
+          Crear Solicitud
+        </button>
+        <Alert :msg="error" />
+      </form>
+    </div>
 
     <!-- Request List -->
     <div v-if="requests" class="bg-white m-2">
       <ul>
-        <li
-          v-for="(request, index) in requests"
-          :key="request.id"
-        >
+        <li v-for="(request, index) in requests" :key="request.id">
           <div class="item-list">
             <p class="p-5">
               <span class="item-list-index p-5">{{ index + 1 }}</span>
-              <span class="p-5">{{ request.quantity }} {{ request.description }}</span>
+              <span class="p-5">
+                {{ request.description }} {{ request.quantity }}
+              </span>
             </p>
           </div>
         </li>
@@ -47,46 +54,52 @@ export default {
   data() {
     return {
       error: null,
-      description: '',
-      quantity: '',
-      requests: []
+      description: "",
+      quantity: "",
+      requests: [],
+      order: null
     };
   },
-  props: ['orderId', 'orderStatus'],
+  props: ["orderId", "orderStatus"],
   methods: {
-      createRequest() {
-        this.$apollo.mutate({
-            mutation: require("@/graphql/CreateRequestMutation.gql"),
-            variables: {
-                data: { description: this.description, quantity: this.quantity }
-            },
-            update: (store, { data: { createRequest } }) => {
-              if (createRequest) {
-                this.requests.push(createRequest);
-              } else {
-                this.error = "Error al crear la orden.";
-              }
+    createRequest() {
+      this.$apollo
+        .mutate({
+          mutation: require("@/graphql/CreateRequestMutation.gql"),
+          variables: {
+            input: {
+              description: this.description,
+              quantity: this.quantity,
+              order: this.orderId
             }
-          })
-          .then(() => {
-            this.error = null;
-          })
-          .catch(error => {
-            this.error = error.message;
-          });
-      }
+          },
+          update: (store, { data: { createRequest } }) => {
+            if (createRequest) {
+              this.requests.push(createRequest);
+            } else {
+              this.error = "Error al crear la orden.";
+            }
+          }
+        })
+        .then(() => {
+          this.error = null;
+        })
+        .catch(error => {
+          this.error = error.message;
+        });
+    }
   },
   components: {
-      Alert
+    Alert
   },
   apollo: {
     requests: {
-        query: require("@/graphql/AllRequestsByOrderQuery.gql"),
-        variables () {
-          return {
-            order_id: this.orderId
-          };
-        }
+      query: require("@/graphql/AllRequestsByOrderQuery.gql"),
+      variables() {
+        return {
+          order_id: this.orderId
+        };
+      }
     }
   }
 };
