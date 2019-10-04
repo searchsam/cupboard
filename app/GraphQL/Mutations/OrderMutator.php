ac56e5ce8ee16ee245d50a527064d3d6efa3b6db
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2019-10-04 16:35:21 -0600

    actualizar ordenes y solicitudes

--- app/GraphQL/Mutations/OrderMutator.php
+++ app/GraphQL/Mutations/OrderMutator.php
@@ -30,10 +30,28 @@ class OrderMutator
      * @param array $args
      * @return mixed
      */
+    public function update($root, array $args)
+    {
+        $orderId = $args['id'];
+        $deadline = $args['deadline'];
+        $name = $args['name'];
+
+        return tap(Order::find($orderId))
+            ->update([
+                'deadline' => $deadline,
+                'name' => $name
+            ]);
+    }
+
+    /**
+     * @param $root
+     * @param array $args
+     * @return mixed
+     */
     public function shop($root, array $args)
     {
         $orderId = $args['id'];
-        
+
         return tap(Order::find($orderId), function ($order) {
             $order->update(['status' => Order::COMPLETED]);
             event(new ShopOrder($order));
