0471e7b57b8a7e95dccb99bc259f93ca6b09656b
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2019-10-30 16:40:21 -0600

    add pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('BROADCAST_DRIVER', 'null'),
+    'default' => env('BROADCAST_DRIVER', 'pusher'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER', null),
                 'useTLS' => true,
                 // 'host' => '127.0.0.1',
-                'port' => 8001,
+                // 'port' => 8001,
                 // 'scheme' => 'http'
             ],
         ],
